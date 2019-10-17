015730438c0a11235d8297674cd1fa29be27bc4e
kevmeath <kevmeath@gmail.com>
2019-10-17 15:04:51 +0100

    Tuple input

---
+++ Lab4/MathsFunctions.py
@@ -0,0 +1,19 @@
+def createTuple(tupleString):
+    for i in range(len(tupleString)):
+        if tupleString[i] == ',':
+            return int(tupleString[:i]), int(tupleString[i + 1:])
+
+# def gcd(large, small):
+
+# def lcm(num1, num2):
+
+# def addFrac(fraction1, fraction2):
+
+# def subFrac(fraction1, fraction2):
+
+# def simplify(fraction):
+
+tuple1 = createTuple(input("Enter tuple 1: "))
+tuple2 = createTuple(input("Enter tuple 2: "))
+print(tuple1, tuple2)
+print(gcd(tuple1[1], tuple1[0]))
