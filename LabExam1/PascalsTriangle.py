f899b02527eb1b9b18ee3ab3cd85091dd06b3ed9
kevmeath <kevmeath@gmail.com>
2019-10-24 14:36:46 +0100

    print Pascal's Triangle up to given height

--- LabExam1/PascalsTriangle.py
+++ LabExam1/PascalsTriangle.py
@@ -2,6 +2,7 @@ def make_new_row(old_row):
     """Requires:
            -- list old_row that begins and ends with a 1 and has zero or more
               integers in between (has to have at least [1,1])
+              OR list [] or [1]
            Returns:
            -- list beginning and ending with a 1 and each interior (non 1)
               integer is the sum of the corresponding old_row elements
@@ -12,17 +13,24 @@ def make_new_row(old_row):
         return [1]
     elif old_row == [1]:
         return [1,1]
+    else:
+        new_row = [1]
 
-    new_row = [1]
+        for i in range(len(old_row) - 1):
+            new_row.append(old_row[i] + old_row[i+1])
 
-    for i in range(len(old_row) - 1):
-        new_row.append(old_row[i] + old_row[i+1])
-
-    new_row.append(1)
+        new_row.append(1)
 
     return new_row
 
 
-row_1 = [1,1]
-row_2 = make_new_row(row_1)
-print(row_2)
+height = int(input("Enter desired height of Pascal's Triangle (greater than 0): "))
+
+while height < 1:
+    height = int(input("Enter a height greater than 0: "))
+
+row = []
+
+for i in range(height):
+    row = make_new_row(row)
+    print(row)
