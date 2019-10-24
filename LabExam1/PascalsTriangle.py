f56d7af403fde2e7fe61f79b2f3c4823b25afcd5
kevmeath <kevmeath@gmail.com>
2019-10-24 14:25:16 +0100

    refactor make_new_row to handle [] and [1]

--- LabExam1/PascalsTriangle.py
+++ LabExam1/PascalsTriangle.py
@@ -8,7 +8,13 @@ def make_new_row(old_row):
               For example if old_row = [1,4,6,4,1], then new_row = [1,5,10,10,5,1],
               i.e. 5=1+4, 10=4+6, 10=6+4, 5=4+1 """
 
+    if not old_row:
+        return [1]
+    elif old_row == [1]:
+        return [1,1]
+
     new_row = [1]
+
     for i in range(len(old_row) - 1):
         new_row.append(old_row[i] + old_row[i+1])
 
@@ -17,6 +23,6 @@ def make_new_row(old_row):
     return new_row
 
 
-row_1 = [1, 1]
+row_1 = [1,1]
 row_2 = make_new_row(row_1)
 print(row_2)
